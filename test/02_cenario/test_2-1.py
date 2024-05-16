6c0555dcc690d708268243e642fb20c8eeafd508
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-05-16 17:00:54 -0300

    Ajustado erro do teste 2-1

--- test/02_cenario/test_2-1.py
+++ test/02_cenario/test_2-1.py
@@ -4,4 +4,4 @@ def test_2_1_igualdade_10():
     print('2º passo do teste feito com Sucesso!')
     print('3º passo do teste feito com Sucesso!')
     print('4º passo do teste feito com Sucesso!')
-    assert 10 == 9    
+    assert 10 == 10    
