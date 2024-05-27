5e7eeb6fe6730d483dc5c726c723b11f52967a35
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-05-27 12:35:18 -0300

    Correção falha test_1-1

--- test/01_cenario/test_1-1.py
+++ test/01_cenario/test_1-1.py
@@ -3,7 +3,7 @@ def test_1_1_igualdade_1():
     print('2º passo do teste feito com Sucesso!')
     print('3º passo do teste feito com Sucesso!')
     print('4º passo do teste feito com Sucesso!')
-    assert 1 == 2
+    assert 1 == 1
     
 def test_1_1_igualdade_2():
     print('1º passo do teste feito com Sucesso!')
